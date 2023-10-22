e3898366d8143fb2cfa938c136836ac29959dbc3
vit0r <vitornascimentoaraujo@gmail.com>
2023-10-22 14:48:43 -0300

    libvirt fix addresses

--- output.tf
+++ output.tf
@@ -1,4 +1,7 @@
-output "domains_informations" {
-  value = libvirt_domain.domain
+output "domain_access_info" {
+  value = {
+    "hostname" : libvirt_domain.domain.*.network_interface.0.hostname,
+    "ip" : libvirt_domain.domain.*.network_interface.0.addresses.0
+  }
 }
 
